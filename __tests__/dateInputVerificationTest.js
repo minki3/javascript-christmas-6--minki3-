ed4dcd5cf75e0c1fb2585feb04facd0d393fe81a
minki3 <86992006+minki3@users.noreply.github.com>
2023-11-14 03:27:28 +0900

    test:입력 갯수 테스트 코드 작성

--- __tests__/dateInputVerificationTest.js
+++ __tests__/dateInputVerificationTest.js
@@ -6,19 +6,19 @@ describe("날짜 입력 예외 테스트", () => {
     const result = dateInputVerification(number);
 
     expect(result).toBe(20);
-  }),
-    it("1 미만 일시 ", () => {
-      const number = 0;
+  });
+  it("1 미만 일시 ", () => {
+    const number = 0;
 
-      expect(() => {
-        dateInputVerification(number);
-      }).toThrow("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-    }),
-    it("31 초과 일시 ", () => {
-      const number = 32;
+    expect(() => {
+      dateInputVerification(number);
+    }).toThrow("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+  });
+  it("31 초과 일시 ", () => {
+    const number = 32;
 
-      expect(() => {
-        dateInputVerification(number);
-      }).toThrow("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-    });
+    expect(() => {
+      dateInputVerification(number);
+    }).toThrow("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+  });
 });
