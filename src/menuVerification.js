ab0503b946fa1dbaef85ad959d4fd51405d24f2f
minki3 <86992006+minki3@users.noreply.github.com>
2023-11-14 02:35:18 +0900

    Feat:음료만 입력할 경우 에러처리 구현

--- src/menuVerification.js
+++ src/menuVerification.js
@@ -1,9 +1,12 @@
 import compareMenu from "./compareMenu.js";
 import menuObject from "./menuObject.js";
 import numberOfMenus from "./numberOfMenus.js";
+import onlyDrinkError from "./onlyDrinkError.js";
 
 export default function menuVerification(menu) {
   const menuList = menuObject(menu);
+  onlyDrinkError(menuList);
   compareMenu(menuList);
   numberOfMenus(menuList);
+  return menuList;
 }
