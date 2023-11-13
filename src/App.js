bc95e9a321a1354c236da48347834861f9638fdc
minki3 <86992006+minki3@users.noreply.github.com>
2023-11-13 18:08:15 +0900

    docs : 기능 구현 목록 정리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import InputView from "./InputView.js";
 class App {
-  async run() {}
+  async run() {
+    console.log(await InputView.readDate());
+    // await inputView.readDate();
+  }
 }
+
+const app = new App();
+app.run();
 
 export default App;
