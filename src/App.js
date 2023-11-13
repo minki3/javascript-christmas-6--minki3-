02343390fcba5274e5612640de24a5ad062b7e1f
minki3 <86992006+minki3@users.noreply.github.com>
2023-11-14 05:15:15 +0900

    Feat:할인 전 금액 계산 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import OutputView from "./OutputView.js";
 import dateInputRetry from "./dateInputRetry.js";
 import menuInputRetry from "./menuInputRetry.js";
 import menuOutput from "./menuOutput.js";
+import regularPrice from "./regularPrice.js";
 
 class App {
   async run() {
     const date = await dateInputRetry();
     const menu = await menuInputRetry();
-    OutputView.printMenu();
     menuOutput(menu);
+    regularPrice(menu);
   }
 }
 
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,8 +1,8 @@
 import { Console } from "@woowacourse/mission-utils";
 
 const OutputView = {
-  printMenu() {
-    Console.print("<주문 메뉴>");
+  printMenu(output) {
+    Console.print(`<${output}>`);
   },
   printError(error) {
     Console.print(`${error}`);
